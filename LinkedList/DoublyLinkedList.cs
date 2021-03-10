50e50bd986c702923666814e1dece9f1ea32339e
Hendrik Kim <48870515+hendrik-kim@users.noreply.github.com>
2021-03-10 14:40:05 -0600

    doubly linked list

--- LinkedList/DoublyLinkedList.cs
+++ LinkedList/DoublyLinkedList.cs
@@ -66,6 +66,7 @@ namespace DataStructurePractice.LinkedList
                     removeNode.Next.Prev = removeNode.Prev;
                 }
             }
+            //
             removeNode = null;
         }
 
