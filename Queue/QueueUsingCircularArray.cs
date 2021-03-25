6545cb912505e91874c7102c1101d8a1a38366d4
Hendrik Kim <48870515+hendrik-kim@users.noreply.github.com>
2021-03-24 21:32:53 -0500

    add 3rd queue method

---
+++ Queue/QueueUsingCircularArray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructurePractice.Queue
+{
+    public class QueueUsingCircularArray
+    {
+        private object[] a;
+        private int front;
+        private int rear;
+
+        public QueueUsingCircularArray(int queueSize)
+        {
+            a = new object[queueSize];
+            front = -1;
+            rear = -1;
+        }
+
+        public void Enqueue(object data)
+        {
+            if ((rear + 1) % a.Length == front)
+            {
+                throw new ApplicationException("Full");
+            }
+            else
+            {
+                if (front == -1)
+                {
+                    front++;
+                }
+                rear = (rear + 1) % a.Length;
+                a[rear] = data;
+            }    
+        }
+
+        public object Dequeue()
+        {
+            if(front == -1 && rear == -1)
+            {
+                throw new ApplicationException("Empty");
+            }
+            else
+            {
+                object data = a[front];
+
+                if(front == rear)
+                {
+                    front = -1;
+                    rear = -1;
+                }
+                else
+                {
+                    front = (front + 1) % a.Length;
+                }
+
+                return data;
+            }
+        }
+    }
+}
---
+++ Queue/QueueUsingCircularArray2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructurePractice.Queue
+{
+    public class QueueUsingCircularArray2
+    {
+        private object[] a;
+        private int front = 0;
+        private int rear = 0;
+
+        public QueueUsingCircularArray2(int queueSize)
+        {
+            a = new object[queueSize];
+        }
+
+        public void Enqueue(object data)
+        {
+            if((rear+1) % a.Length == front)
+            {
+                throw new ApplicationException("Full");
+            }
+            a[rear] = data;
+            rear = (rear + 1) % a.Length;
+        }
+
+        public object Dequeue()
+        {
+            if(front == rear)
+            {
+                throw new ApplicationException("Empty");
+            }
+
+            object data = a[front];
+            front = (front + 1) % a.Length;
+            return data;
+        }
+    }
+}
